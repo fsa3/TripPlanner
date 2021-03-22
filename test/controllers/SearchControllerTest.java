76737e7008598f9b1efdd4f3b07da44adb504a5e
Fannar Steinn Adalsteinsson <fsa3@hi.is>
2021-03-22 19:37:32 +0000

    Test klasi fyrir SearchController búinn til.

---
+++ test/controllers/SearchControllerTest.java
@@ -0,0 +1,26 @@
+package controllers;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SearchControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void createTripPackages() {
+    }
+
+    @Test
+    void getPackages() {
+    }
+}
