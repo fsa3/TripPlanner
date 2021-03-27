2ec253eb4fc99cf627011a5b6bb73a0b202cb28a
Bjorn Borgar Magnusson <bjorn.borgar@gmail.com>
2021-03-27 19:44:35 +0000

    merge ves

--- src/ui/controllers/LoginUiController.java
+++ src/ui/controllers/LoginUiController.java
@@ -7,23 +7,38 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import org.w3c.dom.Text;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
+import java.util.regex.Pattern;
 
 public class LoginUiController implements Initializable {
 
     @FXML
     public Label notMemberLabel;
+    @FXML
+    public TextField firstNameTextField;
+    @FXML
+    public TextField lastNameTextField;
+    @FXML
+    public TextField phoneTextField;
+    @FXML
+    public TextField ssNumberTextField;
+    @FXML
+    public VBox inputs;
     @FXML
     private BorderPane sceneRoot;
     @FXML
@@ -32,6 +47,8 @@ public class LoginUiController implements Initializable {
     public PasswordField passwordInput;
     @FXML
     public Label errorLabel;
+    @FXML
+    private Label signUpErrorLabel;
 
     private User user;
     private SearchUiController owningController;
@@ -76,7 +93,6 @@ public class LoginUiController implements Initializable {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 
     public void notMemberMouseEnter(MouseEvent mouseEvent) {
@@ -85,5 +101,37 @@ public class LoginUiController implements Initializable {
 
     public void notMemberMouseExit(MouseEvent mouseEvent) {
         notMemberLabel.setUnderline(false);
+    }
+
+    public void createUser(ActionEvent actionEvent) {
+        UserController userController = new UserController();
+        ArrayList<String> userInfo = new ArrayList<>();
+        String regex = "^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$";
+        Pattern emailPattern = Pattern.compile(regex);
+        regex = "[0-9]+";
+        Pattern phonePattern = Pattern.compile(regex);
+        signUpErrorLabel.setText("");
+        StringBuilder errorMsg = new StringBuilder("Please enter ");
+        for(Node tf : inputs.getChildren()) {
+            String nodeText = ((TextField)tf).getText();
+            if(nodeText.isEmpty()) {
+                errorMsg.append(tf.getAccessibleText()).append(", ");
+            }
+            userInfo.add(nodeText);
+        }
+        if(errorMsg.length() > 13) signUpErrorLabel.setText(errorMsg.toString().substring(0, errorMsg.length()-2));
+        else if(!emailPattern.matcher(userInfo.get(0)).matches()) {
+            signUpErrorLabel.setText("Invalid email address");
+        }
+        else if(userController.getUser(userInfo.get(0)) != null) {
+            signUpErrorLabel.setText("An account with this email already exists");
+        }
+        else if(!phonePattern.matcher(userInfo.get(4)).matches()) {
+            signUpErrorLabel.setText("Invalid phone number");
+        }
+        else {
+            userController.createUser(userInfo);
+            signUpErrorLabel.setText("User successfully created, please log in");
+        }
     }
 }
