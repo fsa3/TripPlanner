e7f9e00f4f54fed5e13598bde1db1e38c9c6db9d
agustheidar <ahs33@hi.is>
2021-03-28 13:45:01 +0000

    Mock data factory buid til

--- src/data/MockDataFactory.java
+++ src/data/MockDataFactory.java
@@ -11,21 +11,29 @@ import java.util.ArrayList;
 public class MockDataFactory implements Database{
     @Override
     public ArrayList<Flight> getFlights(String depCity, String destCity, LocalDate date) {
-        return null;
+        ArrayList<Flight> flights = new ArrayList<>();
+        flights.add(new Flight("FN-6969", date, depCity, destCity));
+        return flights;
     }
 
     @Override
     public ArrayList<Hotel> getHotels(String city) {
-        return null;
+        ArrayList<Hotel> hotels = new ArrayList<>();
+        hotels.add(new Hotel("Hotel OLV", city));
+        return hotels;
     }
 
     @Override
     public ArrayList<DayTrip> getDayTrips(String city) {
-        return null;
+        ArrayList<DayTrip> dayTrips = new ArrayList<>();
+        dayTrips.add(new DayTrip("Kúrstund at Fannar's Room!", city));
+        return dayTrips;
     }
 
     @Override
     public ArrayList<User> getUsers() {
-        return null;
+        ArrayList<User> users = new ArrayList<>();
+        users.add(new User("ahs33@hi.is", "Ágúst Heiðar", "Sveinbjörnsson", "kallijons", "8975115", "5678"));
+        return users;
     }
 }
