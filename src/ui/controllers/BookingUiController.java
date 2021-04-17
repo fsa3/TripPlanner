ecf23d1bcee19a00245af86519d4d89fd1623720
Bjorn Borgar Magnusson <bjorn.borgar@gmail.com>
2021-04-17 14:55:26 +0000

    css vinnsla

--- src/ui/controllers/BookingUiController.java
+++ src/ui/controllers/BookingUiController.java
@@ -335,6 +335,7 @@ public class BookingUiController {
             Label capacity = new Label(r.getCap() + " people");
             Label price = new Label(r.getPrice() + "$");
             Hyperlink removeRoom = new Hyperlink("Remove room");
+            removeRoom.getStyleClass().add("rrHyperlink");
             roomEntry.getChildren().addAll(roomTitle, capacity, price, removeRoom);
             roomEntry.setSpacing(10);
             roomsVB.getChildren().add(roomEntry);
